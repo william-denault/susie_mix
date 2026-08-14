873b034ff3ad7db2808abf327ed6e713a3cbab73
WD <william-denault@users.noreply.github.com>
2026-08-14 16:26:25 +0200

    adding plots

--- script/analysis/case_study.R
+++ script/analysis/case_study.R
@@ -1,5 +1,160 @@
 
+# Case where SuSiE mix and SuSiE mix agree but SuSiE mix recode variant ----
 
+
+Nerve
+gene_name= "ZNF232"
+out<- readRDS(paste0("/project2/mstephens/wdenault/susie_mix/results/",gene_name,".rds"))
+names(out)
+k=14
+par(mfrow=c(1,2))
+
+
+susie_plot(out[[k]]$susie_add, y="PIP", main=paste( gene_name, names(out)[k], "addivite"))
+susie_plot(out[[k]]$susie_mix, y="PIP", main=paste( gene_name, names(out)[k], "mix"))
+
+
+abline(v=out[[k]]$n_SNP+1, col="red", lty=2)
+abline(v=2*out[[k]]$n_SNP+1, col="red", lty=2)
+par(mfrow=c(1,1))
+out[[k]]$mean_read
+
+out[[k]]$n_SNP
+
+out[[k]]$susie_add$sets
+
+out[[k]]$susie_mix$sets
+
+2*out[[k]]$n_SNP+out[[k]]$susie_add$sets$cs$L2
+
+
+
+## leaninng torward dominant ----
+
+gene_name= "SCAMP5"
+out<- readRDS(paste0("/project2/mstephens/wdenault/susie_mix/results/",gene_name,".rds"))
+names(out)
+k=8
+par(mfrow=c(1,2))
+
+
+susie_plot(out[[k]]$susie_add, y="PIP", main=paste( gene_name, names(out)[k], "addivite"))
+susie_plot(out[[k]]$susie_mix, y="PIP", main=paste( gene_name, names(out)[k], "mix"))
+
+
+abline(v=out[[k]]$n_SNP+1, col="red", lty=2)
+abline(v=2*out[[k]]$n_SNP+1, col="red", lty=2)
+par(mfrow=c(1,1))
+out[[k]]$mean_read
+
+out[[k]]$n_SNP
+
+out[[k]]$susie_add$sets
+
+out[[k]]$susie_mix$sets
+
+
+
+out[[k]]$n_SNP+out[[k]]$susie_add$sets$cs$L1
+
+
+
+
+
+### Case where SuSiE cannot pick between variant  -----
+
+gene_name= "SCAMP5"
+out<- readRDS(paste0("/project2/mstephens/wdenault/susie_mix/results/",gene_name,".rds"))
+names(out)
+k=9
+par(mfrow=c(1,2))
+
+
+susie_plot(out[[k]]$susie_add, y="PIP", main=paste( gene_name, names(out)[k], "addivite"))
+susie_plot(out[[k]]$susie_mix, y="PIP", main=paste( gene_name, names(out)[k], "mix"))
+
+
+abline(v=out[[k]]$n_SNP+1, col="red", lty=2)
+abline(v=2*out[[k]]$n_SNP+1, col="red", lty=2)
+par(mfrow=c(1,1))
+out[[k]]$mean_read
+
+out[[k]]$n_SNP
+
+out[[k]]$susie_add$sets
+
+out[[k]]$susie_mix$sets
+
+
+
+out[[k]]$n_SNP+out[[k]]$susie_add$sets$cs$L1
+
+
+## Nice secondary dominat secondary signal ----
+
+gene_name= "GTF2H2"
+out<- readRDS(paste0("/project2/mstephens/wdenault/susie_mix/results/",gene_name,".rds"))
+names(out)
+k=6
+par(mfrow=c(1,2))
+
+
+susie_plot(out[[k]]$susie_add, y="PIP", main=paste( gene_name, names(out)[k], "addivite"))
+susie_plot(out[[k]]$susie_mix, y="PIP", main=paste( gene_name, names(out)[k], "mix"))
+
+
+abline(v=out[[k]]$n_SNP+1, col="red", lty=2)
+abline(v=2*out[[k]]$n_SNP+1, col="red", lty=2)
+par(mfrow=c(1,1))
+out[[k]]$mean_read
+
+out[[k]]$n_SNP
+
+out[[k]]$susie_add$sets
+
+out[[k]]$susie_mix$sets
+
+
+
+
+
+
+
+
+## Nice case where SuSiE additive is quite differnet ----
+
+gene_name= "HPR"
+out<- readRDS(paste0("/project2/mstephens/wdenault/susie_mix/results/",gene_name,".rds"))
+names(out)
+k=4
+par(mfrow=c(1,2))
+
+
+susie_plot(out[[k]]$susie_add, y="PIP", main=paste( gene_name, names(out)[k], "addivite"))
+susie_plot(out[[k]]$susie_mix, y="PIP", main=paste( gene_name, names(out)[k], "mix"))
+
+
+abline(v=out[[k]]$n_SNP+1, col="red", lty=2)
+abline(v=2*out[[k]]$n_SNP+1, col="red", lty=2)
+par(mfrow=c(1,1))
+out[[k]]$mean_read
+
+out[[k]]$n_SNP
+
+out[[k]]$susie_add$sets
+
+out[[k]]$susie_mix$sets
+
+
+
+out[[k]]$n_SNP+out[[k]]$susie_add$sets$cs$L1
+
+
+
+
+
+
+### OLD------
 ## Nice secondary recessive secondary signal ----
 out<- readRDS("/project2/mstephens/wdenault/susie_mix/results/BIRC7.rds")
 names(out)
