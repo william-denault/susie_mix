161d5ddcfa3c86522e9905d3532e64fc54bb6f9a
WD <william-denault@users.noreply.github.com>
2026-09-08 00:03:43 +0200

    Update raw_data_plot_2CS_signal.R

--- script/analysis/raw_data_plot_2CS_signal.R
+++ script/analysis/raw_data_plot_2CS_signal.R
@@ -1,13 +1,25 @@
+source("/project2/mstephens/wdenault/susie_mix/script/analysis/descriptive results.R", echo=TRUE)
 
 
+table( res_idx$ncs_susie, res_idx$ncs_susie_mix)
+
+
+
+res_idx[ which(res_idx$ncs_susie==1 & res_idx$ncs_susie_mix==3), 1:20]
+
+tt= res_idx[ which(res_idx$ncs_susie< res_idx$ncs_susie_mix), 1:20]
+
+
+
+
+tt= res_idx[ which(res_idx$ncs_susie==1 &res_idx$ncs_susie_mix==2), 1:20]
 ## Nice secondary dominat secondary signal ----
-
-gene_name= "GTF2H2"
+l=1
+gene_name= tt$gene[l]
 out<- readRDS(paste0("/project2/mstephens/wdenault/susie_mix/results/",gene_name,".rds"))
 names(out)
-k=6
+k=which(names(out)==tt$tissue[l])
 par(mfrow=c(1,2))
-names(out)[6]
 
 susie_plot(out[[k]]$susie_add, y="PIP", main=paste( gene_name, names(out)[k], "addivite"))
 susie_plot(out[[k]]$susie_mix, y="PIP", main=paste( gene_name, names(out)[k], "mix"))
@@ -16,6 +28,9 @@ susie_plot(out[[k]]$susie_mix, y="PIP", main=paste( gene_name, names(out)[k], "m
 abline(v=out[[k]]$n_SNP+1, col="red", lty=2)
 abline(v=2*out[[k]]$n_SNP+1, col="red", lty=2)
 par(mfrow=c(1,1))
+
+
+l=l+1
 out[[k]]$mean_read
 
 out[[k]]$n_SNP
