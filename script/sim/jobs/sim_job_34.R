1bbcaf753c477321ca23972b0c413325ad2a32ad
WD <william-denault@users.noreply.github.com>
2026-09-09 15:37:59 +0200

    continued cleaning

--- script/sim/jobs/sim_job_34.R
+++ script/sim/jobs/sim_job_34.R
@@ -1,13 +1,13 @@
 rm(list = ls())
 source("/project2/mstephens/wdenault/susie_mix/script/sim/sim_workhorse.R")
 dir.create("/project2/mstephens/wdenault/susie_mix/simulation results/chunks/", recursive = TRUE, showWarnings = FALSE)
-if (file.exists("/project2/mstephens/wdenault/susie_mix/simulation results/chunks/additive_add2_rec0_dom0_n500_L10_pve0.2_seed1e+06_reps200_chunk1.RData")) {
-  load("/project2/mstephens/wdenault/susie_mix/simulation results/chunks/additive_add2_rec0_dom0_n500_L10_pve0.2_seed1e+06_reps200_chunk1.RData")
+if (file.exists("/project2/mstephens/wdenault/susie_mix/simulation results/chunks/additive_add2_rec0_dom0_n500_L10_pve0.2_seed1e+06_reps400_chunk1.RData")) {
+  load("/project2/mstephens/wdenault/susie_mix/simulation results/chunks/additive_add2_rec0_dom0_n500_L10_pve0.2_seed1e+06_reps400_chunk1.RData")
 } else {
   results <- list()
 }
 start <- length(results) + 1
-if (start <= 200) for (o in start:200) {
+if (start <= 400) for (o in start:400) {
   replication <- 0 + o
   sim_seed <- 1e+06 + replication
   temp <- tryCatch(
@@ -17,6 +17,6 @@ if (start <= 200) for (o in start:200) {
   temp$seed <- sim_seed
   temp$replication <- replication
   results[[o]] <- temp
-  save(results, file = "/project2/mstephens/wdenault/susie_mix/simulation results/chunks/additive_add2_rec0_dom0_n500_L10_pve0.2_seed1e+06_reps200_chunk1.RData.tmp")
-  if (!file.rename("/project2/mstephens/wdenault/susie_mix/simulation results/chunks/additive_add2_rec0_dom0_n500_L10_pve0.2_seed1e+06_reps200_chunk1.RData.tmp", "/project2/mstephens/wdenault/susie_mix/simulation results/chunks/additive_add2_rec0_dom0_n500_L10_pve0.2_seed1e+06_reps200_chunk1.RData")) stop("Could not replace the checkpoint.")
+  save(results, file = "/project2/mstephens/wdenault/susie_mix/simulation results/chunks/additive_add2_rec0_dom0_n500_L10_pve0.2_seed1e+06_reps400_chunk1.RData.tmp")
+  if (!file.rename("/project2/mstephens/wdenault/susie_mix/simulation results/chunks/additive_add2_rec0_dom0_n500_L10_pve0.2_seed1e+06_reps400_chunk1.RData.tmp", "/project2/mstephens/wdenault/susie_mix/simulation results/chunks/additive_add2_rec0_dom0_n500_L10_pve0.2_seed1e+06_reps400_chunk1.RData")) stop("Could not replace the checkpoint.")
 }
